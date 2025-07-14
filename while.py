6c8b0575a029a31f61860e486a907d47727a1db7
KID-DEE <danieldaliya18@gmail.com>
2025-07-14 10:48:27 +0100

    hi

---
+++ while.py
@@ -0,0 +1,34 @@
+"""
+14th july 2025
+lead : OLatunbosun Adeniyi
+9am-11am
+Implementing "while" in python
+"""
+
+number = float(input("please enter the number of iteraction you want to perform:"))
+d =0
+# while d<=int(number):
+#  name = input("please enter your name:")
+#  print(d,"",name)
+#  d +=1
+#  print("you entered value is:", number)
+
+
+# # implement break a exit from the code while d<=number
+# while d<=int(number):
+#   name = input("please enter your name:  ")
+#   print(d,"",name)
+#   d +=1
+ 
+#   if d>=2:
+#    break
+#   print("you entered value is:", number)
+
+#iimplement continue, skip a line that meet the if condition criteria
+while d<=int(number):
+ name = input("please enter your name:")
+ d+=1
+ if d==2:     
+     continue
+ print(d,"",name)
+print("you entered value is:", number)
