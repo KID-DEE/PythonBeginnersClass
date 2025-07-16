fd5a93cb3ebb902e7e9991610b3c68a1964fc561
KID-DEE <danieldaliya18@gmail.com>
2025-07-16 10:06:57 +0100

    dd is going places

---
+++ if...else.py
@@ -0,0 +1,20 @@
+"""
+16th july 2025
+Lead:Olatunbosun Adeniyi
+9am-11am
+Iplementing "if..else" in python
+"""
+
+value=(input("please enter random value between 1 & 20: "))
+number=int(value)
+name = str(value)
+# if value >=10:
+#     print("The value is greater than 10 your enter value is", value)
+# else:
+#     print("The value is less than condition value")
+
+print("\n Iterating long if statements")
+if number<=10:
+    print("Hi your value/grade is: A")
+else:
+    print("please check your input for correct value")
