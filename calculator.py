b4e3b98a699775db9bdf8d0adc23f3a500e1a693
KID-DEE <danieldaliya18@gmail.com>
2025-07-17 10:07:27 +0100

    making lots of progress

--- calculator.py
+++ calculator.py
@@ -3,22 +3,22 @@ print("I am your Simple Calculator")
 print("Good Day")
 name = input("please enter your full name:")
 print("welcome",name,"lets perform some operations")
-main=int(input("Select Operation: \n1,Addition \n2, Substraction \n3,Multiplication" \
-" \n4,Division \n5,Modulus \n6,Floor division \ninput here: "))
+main=input("Select Operation: \n1,Addition \n2, Substraction \n3,Multiplication" \
+" \n4,Division \n5,Modulus \n6,Floor division \ninput here: ")
 try:
     x = int(input("Enter a number:"))
     y = int(input("Enter second number: "))
-    if main==1:
+    if int(main)==1:
         print("Your Result is:",x+y)
-    elif main==2: 
+    elif int(main)==2: 
         print("Your Result is:",float(x)-float(y))
-    elif main==3:
+    elif int(main)==3:
         print("Your Result is:",float(x)*float(y))
-    elif main==4:
+    elif int(main)==4:
         print("Your Result is:",float(x)/float(y))
-    elif main==5:
+    elif int(main)==5:
         print("Your Result is:",float(x)%float(y))
-    elif main==6:
+    elif int(main)==6:
         print("Your result is:",float(x)//float(y))
     else:
         print("Invalid Syntax")
